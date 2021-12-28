291337bfb8c84e71ca4451c01ec35686ba3b05f2
TheEyesightDim <anisohedron@gmail.com>
2021-12-28 18:01:24 -0500

    added /dbg console command

--- modules/console_commands.mjs
+++ modules/console_commands.mjs
@@ -1,9 +1,30 @@
+import { msg_dispatcher } from "./dispatchers.mjs";
+
 function shutdown() {
   process.kill(process.pid, "SIGTERM");
 }
 
+async function dbg(...args) {
+  //allows for debugging chat commands "silently" with /dbg !cmd args...
+
+  const channel = "<<Debug>>";
+  const user = {
+    "display-name": "dbg",
+  };
+  const msg = args.join(" ");
+  const is_self = false;
+  const command = args[0];
+  const content = args.slice(1).join(" ");
+  const client = {
+    say: console.log,
+  };
+
+  await msg_dispatcher(client)(channel, user, msg, is_self);
+}
+
 const console_commands = {
   "/shutdown": shutdown,
+  "/dbg": dbg,
 };
 
 export function console_call(input) {
